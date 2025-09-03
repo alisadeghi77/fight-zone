9a13210098d539427c21aa39b22a89cec51e9c6c
Sadeghii.Ali <Sadeghii.Ali@okala.com>
2025-09-03 21:39:20 +0330

    init

---
+++ src/app/app-config.ts
@@ -0,0 +1,9 @@
+export class BerryConfig {
+  static isCollapse_menu = false;
+  static font_family = 'Roboto'; // Roboto, poppins, inter
+  static layout: 'compact'
+  static isDarkMode = true;
+  static isRtl_layout = true
+
+
+}
