ae184e503679c8941e6d08dade6b5f177b88519c
Sadeghii.Ali <Sadeghii.Ali@okala.com>
2025-10-10 17:39:49 +0330

    feat: change style

--- src/app/web/web-module.ts
+++ src/app/web/web-module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebRoutingModule } from './web-routing.module';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    WebRoutingModule
+    WebRoutingModule,
+    NgbCarouselModule
   ]
 })
 export class WebModule { }
